323fbe0f92d03047bad8ff159ad08b2d003b2052
RickyTicky <Rickyabrooks92@gmail.com>
2023-03-20 20:27:45 -0400

    checking github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CameraPreview from "./components/camera_preview";
 
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <CameraPreview />
   </React.StrictMode>
 );
 
